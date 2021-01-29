af31650b255e9d80d1e77708be9e692e3d09616d
mdabrowski25 <m.dabrowski97@o2.pl>
2021-01-29 19:57:44 +0100

    fix

--- src/pl/pjwstk/build/Main.java
+++ src/pl/pjwstk/build/Main.java
@@ -2,7 +2,6 @@ package pl.pjwstk.build;
 
 public class Main {
     public static void main(String[] args) {
-
         ContainerGenerator containerGenerator = new ContainerGenerator();
         UniversalContainer universalContainer = containerGenerator.generateUniversalContainer();
         System.out.println(universalContainer.toString());
