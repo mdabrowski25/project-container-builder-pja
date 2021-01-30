5ebfc86f436b326c7cd1abd765b7753129540bf2
mdabrowski25 <m.dabrowski97@o2.pl>
2021-01-30 12:18:38 +0100

    generator + randomEnum simplify

--- src/pl/pjwstk/build/ContainerGenerator.java
+++ src/pl/pjwstk/build/ContainerGenerator.java
@@ -6,9 +6,10 @@ public class ContainerGenerator {
     Random random = new Random();
     String[] dryProductTypes = {"Currier posts", "pallets", "chests", "cartoons",
             "furniture", "PC parts", "plastic", "toys"};
+    String[] gasProductTypes = {"LPG", "gasoline", "diesel", "LNG", "CNG", "nitrogen", "oil"};
 
     public UniversalContainer generateUniversalContainer() {
-        ContainerSizeType randomizedSize = randomEnum(ContainerSizeType.class);
+        ContainerSizeType randomizedSize = randomSizeEnum();
         int randomizedWeight = random.nextInt(1500);
         int randomizedNumberOfPackages = random.nextInt(4500);
         String randomizedDryProductType = dryProductTypes[random.nextInt(dryProductTypes.length)];
@@ -17,8 +18,23 @@ public class ContainerGenerator {
                 randomizedNumberOfPackages, randomizedDryProductType);
     }
 
-    private <T extends Enum<?>> T randomEnum(Class<T> clazz){
-        int x = random.nextInt(clazz.getEnumConstants().length);
-        return clazz.getEnumConstants()[x];
+    public GasContainer generateGasContainer() {
+        ContainerSizeType randomizedSize = randomSizeEnum();
+        int randomizedWeight = random.nextInt(2700);
+        String randomizedGasProductType = gasProductTypes[random.nextInt(gasProductTypes.length)];
+
+        return new GasContainer(randomizedWeight, randomizedSize, randomizedGasProductType);
+    }
+
+    private ContainerSizeType randomSizeEnum() {
+        int i = random.nextInt(4);
+        switch (i) {
+            case 1:
+                return ContainerSizeType.c20;
+            case 2:
+                return ContainerSizeType.c40hc;
+            default:
+                return ContainerSizeType.c40;
+        }
     }
 }
